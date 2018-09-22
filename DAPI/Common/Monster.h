2e23b9907d8f0dd4b501b6f1c2f37b261adba69c
NiteKat <dcm254@gmail.com>
2018-09-22 15:54:21 -0400

    Added comments

--- DAPI/Common/Monster.h
+++ DAPI/Common/Monster.h
@@ -16,19 +16,24 @@ namespace DAPI
 			return (this->my_monster) == (other.my_monster);
 		}
 		
-
+		//If the monster is moving, it returns the x value of the tile
+		//to which they are moving.
 		auto futurex() {
 			if (my_monster)
 				return my_monster->_mfutx;
 			return -1;
 		}
 
+		//If the monster is moving, it returns the x value of the tile
+		//to which they are moving.
 		auto futurey() {
 			if (my_monster)
 				return my_monster->_mfuty;
 			return -1;
 		}
 
+		//Returns the current hit point value of the monster.
+		//NOTE: THIS WILL PROBABLY BE REMOVED BEFORE 1.0.
 		auto hitPoints() {
 			if (my_monster)
 				return my_monster->_mhitpoints;
@@ -36,6 +41,8 @@ namespace DAPI
 				return -1;
 		}
 
+		//Returns the index at which this monster is stored in the monster
+		//array.
 		int id() {
 			auto monster = reinterpret_cast<MonsterStruct(*)[200]>(0x64D330);
 			for (int i = 0; i < 200; i++)
@@ -46,6 +53,7 @@ namespace DAPI
 			return -1;
 		}
 
+		//Returns true if this monster is alive and visible.
 		bool isAlive() {
 			if (my_monster && isVisible())
 				return 0 < my_monster->_mhitpoints;
@@ -53,17 +61,22 @@ namespace DAPI
 				return false;
 		}
 
+		//Returns true if the pointer is not null.
 		bool isValid() {
 			if (my_monster)
 				return true;
 			return false;
 		}
 
+		//Returns true if the tile on which the monster is standing is flagged
+		//as visible by Diablo.
 		bool isVisible() {
 			auto dFlags = reinterpret_cast<char(*)[112][112]>(0x5C6910);
 			return (*dFlags)[my_monster->_mx][my_monster->_my] & 0x40;
 		}
 
+		//Returns the mode stored in the monster. For example, you can use this
+		//to determine if the monster is actively attacking someone.
 		MON_MODE mode() {
 			if (my_monster)
 				return static_cast<MON_MODE>(my_monster->_mmode);
@@ -71,6 +84,7 @@ namespace DAPI
 				return MON_MODE::MM_INVALID;
 		}
 
+		//Returns the name of the monster.
 		std::string name() {
 			if (my_monster)
 				return static_cast<std::string>(my_monster->mName);
@@ -78,6 +92,7 @@ namespace DAPI
 				return static_cast<std::string>("Invalid Monster");
 		}
 
+		//Returns the type of the monster.
 		_monster_id type() {
 			if (my_monster)
 				return static_cast<_monster_id>(my_monster->MType->mtype);
