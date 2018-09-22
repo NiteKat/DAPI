e89909e10a3efc86edf8a436523b8c4ae0b6a07f
NiteKat <dcm254@gmail.com>
2018-09-22 16:13:43 -0400

    Added comments.

--- DAPI/Common/Object.h
+++ DAPI/Common/Object.h
@@ -16,6 +16,7 @@ namespace DAPI
 			return (this->my_object) == (other.my_object);
 		}
 
+		//Returns the index at which the object is stored in the object array.
 		int id() {
 			auto object = reinterpret_cast<ObjectStruct(*)[127]>(0x679C38);
 			for (int i = 0; i < 200; i++)
@@ -26,22 +27,30 @@ namespace DAPI
 			return -1;
 		}
 
+		//Returns if the object is solid.
 		bool isSolid() { return my_object->_oSolidFlag; }
 
+		//Returns true if the pointer is not null.
 		bool isValid() {
 			bool valid = my_object;
 			return valid;
 		}
 
+		//Returns true if the tile on which this object exists is flagged as visible
+		//by Diablo.
 		bool isVisible() {
 			auto dFlags = reinterpret_cast<char(*)[112][112]>(0x5C6910);
 			return (*dFlags)[my_object->_ox][my_object->_oy] & 0x40;
 		}
 
+		//Returns true if the object is selectable, meaning if you mouse over it, does it
+		//display information in the info box, and can you click it to interact with it.
 		bool selectable() {
 			return my_object->_oSelFlag;
 		}
 
+		//If this is a shrine, it will return the type associated with the shrine. You can
+		//use the shrine_type enum to compare.
 		shrine_type shrineType() {
 			if (my_object && static_cast<_object_id>(my_object->_otype) == _object_id::OBJ_SHRINEL || static_cast<_object_id>(my_object->_otype) == _object_id::OBJ_SHRINER) {
 				return static_cast<shrine_type>(my_object->_oVar1);
@@ -50,6 +59,7 @@ namespace DAPI
 				return shrine_type::NONE;
 		}
 
+		//Returns the type of the object.
 		_object_id type() {
 			if (my_object)
 			{
@@ -63,7 +73,6 @@ namespace DAPI
 			}
 		}
 
-		//Need to change x and y to limit to onscreen.
 		int x() {
 			if (my_object)
 				return my_object->_ox;
