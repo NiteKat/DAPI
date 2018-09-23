a95c36d44ef0f9efef25295f8000edb0982c0226
NiteKat <dcm254@gmail.com>
2018-09-22 21:25:06 -0400

    Added comments.

--- DAPI/Common/Towner.h
+++ DAPI/Common/Towner.h
@@ -9,6 +9,7 @@ namespace DAPI
 
 		Towner(TownerStruct* new_towner) { my_towner = new_towner; }
 
+		//Returns the index at which the towner is stored in the towner array.
 		int id() {
 			static auto towner = reinterpret_cast<TownerStruct(*)[16]>(0x6AAC38);
 			for (int i = 0; i < 16; i++)
@@ -19,12 +20,14 @@ namespace DAPI
 			return -1;
 		}
 
+		//Returns true if the poitner is not null.
 		bool isValid() {
 			if (my_towner)
 				return true;
 			return false;
 		}
 
+		//Returns the name of the towner.
 		std::string name() {
 			if (my_towner)
 				return my_towner->_tName;
@@ -32,6 +35,9 @@ namespace DAPI
 				return "INVALID";
 		}
 
+		//Returns the type of towner this is. Note that Griswold's type is the
+		//same as what's used for invalid towners, so you'll want to check for
+		//his name too.
 		_talker_id type() {
 			if (my_towner)
 				return static_cast<_talker_id>(my_towner->_ttype);
